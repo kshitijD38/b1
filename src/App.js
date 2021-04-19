0c6acadf1359e9b25cc8c0227365bb48f657e3c7
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-04-19 22:40:27 +0530

    added input tag

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import "./styles.css";
 
 export default function App() {
   const [b, setB] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
       <div style={{ textAlign: "center" }} className="App">
+        {!b ? (
+          <input placeholder="input here" onChange={handleChange}></input>
+        ) : null}
         <button
           onClick={() => {
             !b ? setB(true) : setB(false);
@@ -15,11 +24,13 @@ export default function App() {
         </button>
         {b ? (
           <div>
-            <h1>Hello CodeSandbox</h1>
+            <h1>{`Welcome ${text} to CodeSandbox`}</h1>
             <h2>Start editing to see some magic happen!</h2>
           </div>
         ) : (
-          <h1>Click above button to Start.</h1>
+          <>
+            <h1>Click above button to Start.</h1>
+          </>
         )}
       </div>
     </>
