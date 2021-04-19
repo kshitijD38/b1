392801f62a3b6841d75310b351e35f13f3a47a6f
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-04-19 22:00:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [b, setB] = useState(false);
+  return (
+    <>
+      <div style={{ textAlign: "center" }} className="App">
+        <button
+          onClick={() => {
+            !b ? setB(true) : setB(false);
+          }}
+        >
+          {b ? `Disappear` : `Appear`}
+        </button>
+        {b ? (
+          <div>
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+          </div>
+        ) : (
+          <h1>Click above button to Start</h1>
+        )}
+      </div>
+    </>
+  );
+}
